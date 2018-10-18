9dec248fca30ed1544e8d1eea501bd4452b45265
Luis Gonzalez <luis.gonzalez@keepler.io>
2018-10-18 18:12:23 +0200

    Adding binary and chunksize in bytes param

--- copymultipart.go
+++ copymultipart.go
@@ -24,17 +24,17 @@ func calculate_limits(lowlimit int64, hilimit int64) string {
 }
 
 func copyPart(params *s3.UploadPartCopyInput, partNumber int, client *s3.S3, notify chan<- s3.CompletedPart) {
-	fmt.Println("STARTING", partNumber, *params.CopySourceRange)
+	fmt.Println("STARTING CHUNK", partNumber, *params.CopySourceRange)
 	respUploadPartCopy, err1 := client.UploadPartCopy(params)
 	if err1 != nil {
 		fmt.Println("ERROR UploadPartCopy", err1)
 		panic(err1)
 	}
-	fmt.Println("SUCCESSS CopyPartResult", partNumber, *respUploadPartCopy.CopyPartResult.ETag)
+	fmt.Println("SUCCESS CHUNK", partNumber, *respUploadPartCopy.CopyPartResult.ETag)
 	notify <- s3.CompletedPart{ETag: aws.String(*respUploadPartCopy.CopyPartResult.ETag), PartNumber: aws.Int64(int64(partNumber))}
 }
 
-func copy_object(sourceBucketName string, sourcePrefix string, destBucketName string, destPrefix string) {
+func copy_object(chunkSize int64, sourceBucketName string, sourcePrefix string, destBucketName string, destPrefix string) {
 	sess, err := session.NewSession(&aws.Config{
 	    Region: aws.String("eu-west-1")},
 	)
@@ -44,7 +44,7 @@ func copy_object(sourceBucketName string, sourcePrefix string, destBucketName st
 		fmt.Println("ERROR ListObjects", err)
 		return
 	}
-	fmt.Println("size", *result.Contents[0].Size, "key", *result.Contents[0].Key)
+	fmt.Println("TOTAL SIZE", *result.Contents[0].Size, "KEY", *result.Contents[0].Key)
 
 	paramsMultipartUpload := &s3.CreateMultipartUploadInput{
 		Bucket: aws.String(destBucketName),
@@ -53,12 +53,13 @@ func copy_object(sourceBucketName string, sourcePrefix string, destBucketName st
 
 	total_size := *result.Contents[0].Size
 
-	max_chunk_size := int64(1000000000)
+	//max_chunk_size := int64(200000000)
+	max_chunk_size := chunkSize
 
 	total_chunks := int(math.Floor(float64(total_size) / float64(max_chunk_size)))
-	fmt.Println("total chunks", total_chunks)
+	fmt.Println("TOTAL CHUNKS", total_chunks)
 	remainer_chunk := total_size % max_chunk_size
-	fmt.Println("remainder bytes", remainer_chunk)
+	fmt.Println("LAST CHUNK Remainder bytes", remainer_chunk)
 	resp, err := client.CreateMultipartUpload(paramsMultipartUpload)
 
 	if err != nil {
@@ -66,7 +67,7 @@ func copy_object(sourceBucketName string, sourcePrefix string, destBucketName st
 		panic(err)
 	}
 
-	fmt.Println("uploadid", *resp.UploadId)
+	fmt.Println("S3 Multipart UploadId", *resp.UploadId)
 
 	outputChan := make(chan s3.CompletedPart)
 
@@ -79,7 +80,7 @@ func copy_object(sourceBucketName string, sourcePrefix string, destBucketName st
 		} else {
 			res = calculate_limits((int64(i)*max_chunk_size)+1, int64(i+1)*max_chunk_size)
 		}
-		fmt.Println(i, "uploading chunk", i, res)
+		fmt.Println("UPLOAD START CHUNK ", i, res)
 
 		paramsUploadInput := &s3.UploadPartCopyInput{
 			Bucket:          aws.String(destBucketName),
@@ -108,7 +109,7 @@ func copy_object(sourceBucketName string, sourcePrefix string, destBucketName st
 		select {
 		case mensajito := <-outputChan:
 			done_chunks++
-			fmt.Println(*mensajito.PartNumber, *mensajito.ETag, done_chunks, total_chunks)
+			//fmt.Println(*mensajito.PartNumber, *mensajito.ETag, done_chunks, total_chunks)
 			unord_parts[*mensajito.PartNumber] = *mensajito.ETag
 		case <-timeout:
 			fmt.Println("Error, time out for copy time...")
@@ -141,12 +142,19 @@ func main() {
 
 	argsWithoutProg := os.Args[1:]
 
-	if len(argsWithoutProg) < 4 {
+	if len(argsWithoutProg) < 5 {
 		fmt.Println("Error, not enough parameters usage:")
-		fmt.Println("copymultipart <source_bucket> <source_key> <dest_bucket> <dest_key>")
+		fmt.Println("./copymultipart <chunksize> <source_bucket> <source_key> <dest_bucket> <dest_key>")
+		fmt.Println("./copymultipart 200000000 bucket1 path/to/keysource bucket2 path/to/key2")
 		os.Exit(-1)
 	}
 
-	copy_object(argsWithoutProg[0], argsWithoutProg[1], argsWithoutProg[2], argsWithoutProg[3])
+	i, err := strconv.ParseInt(argsWithoutProg[0], 10, 64)
+
+	if err != nil {
+		panic(err)
+	} else {
+		copy_object(i, argsWithoutProg[1], argsWithoutProg[2], argsWithoutProg[3], argsWithoutProg[4])	
+	}
 
 }
